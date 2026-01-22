e91f00a28bcefd3c9c57313453e4c6167eafac35
HIT-ITadmin <IT@human-i-t.org>
2026-01-22 15:34:24 -0600

    path

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ import expressLayouts from 'express-ejs-layouts';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -22,8 +19,8 @@ app.use(express.json());
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 app.set('layout', 'layout');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.resolve('views'));
+app.use(express.static(path.resolve('public')));
 
 // Initialize Firebase
 const serviceAccount = {
